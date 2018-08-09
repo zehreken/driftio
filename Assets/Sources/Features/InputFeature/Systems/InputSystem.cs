baf0662229e433476fcead5286e1b266f8155ec3
zehreken <zehreken@gmail.com>
2018-08-09 21:37:28 +0300

    Update input system

--- Assets/Sources/Features/InputFeature/Systems/InputSystem.cs
+++ Assets/Sources/Features/InputFeature/Systems/InputSystem.cs
@@ -6,12 +6,13 @@ namespace cln
     public sealed class InputSystem : IExecuteSystem
     {
         private IContext<GameEntity> _context;
-        
+
         public InputSystem(IContext<GameEntity> context)
         {
             _context = context;
         }
-        
+
+#if UNITY_EDITOR
         public void Execute()
         {
             if (Input.GetMouseButtonDown(0))
@@ -23,5 +24,21 @@ namespace cln
                 _context.CreateEntity().AddInput(InputType.Right);
             }
         }
+#elif UNITY_ANDROID
+        public void Execute()
+        {
+            if (Input.touchCount <= 0)
+                return;
+
+            if (Input.GetTouch(0).position.x <= Screen.width / 2f)
+            {
+                _context.CreateEntity().AddInput(InputType.Left);
+            }
+            else
+            {
+                _context.CreateEntity().AddInput(InputType.Right);
+            }
+        }
+#endif
     }
 }
